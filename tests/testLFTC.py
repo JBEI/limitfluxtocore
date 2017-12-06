b8a0e6e95cda13aa90b1791124c604235fe2cb70
januusingh <jahnavis@berkeley.edu>
2017-12-06 11:48:49 -0800

    updated unit test

--- tests/testLFTC.py
+++ tests/testLFTC.py
@@ -2,6 +2,7 @@ import pytest
 import cobra
 import cobra.test
 import lftc
+from pytest import approx
 
 def test_limitFluxToCore():
     model = cobra.test.create_test_model("ecoli")
@@ -16,29 +17,31 @@ def test_limitFluxToCore():
 
     fluxIntoCore, producingFluxes, consumingFluxes = lftc.limitFluxToCore(exampleCore, model)
 
+    biomass_rxn = model.reactions.get_by_id("Ec_biomass_iJO1366_WT_53p95M")
+    biomass_rxn.objective_coefficient
+
     nonZeroReactions = [r for r in producingFluxes if r > 0]
-    assert(len(nonZeroReactions)) == 2
-    with pytest.raises(AssertionError):
-        len(nonZeroReactions) != 2
-    assert nonZeroReactions[0] == pytest.approx(6.0519877877298019e-32, abs=0.001e-32), 'Different Reaction Fluxes'
-    with pytest.raises(AssertionError):
-        nonZeroReactions[0]!= pytest.approx(6.0519877877298019e-32, abs=0.001e-32)
-    assert nonZeroReactions[1] == pytest.approx(9.8842775465710206e-16, abs=0.001e-16),'Different Reaction Fluxes'
-    with pytest.raises(AssertionError):
-        nonZeroReactions[1]!= pytest.approx(9.8842775465710206e-16, abs=0.001e-16)
-    assert fluxIntoCore == pytest.approx(1.1372958348092502e-15, abs=0.001e-15), 'Different fluxIntoCore'
-    with pytest.raises(AssertionError):
-        fluxIntoCore != pytest.approx(1.1372958348092502e-15, abs=0.001e-15)
-    assert len(consumingFluxes) == 48, 'Different consumingFlux'
-    with pytest.raises(AssertionError):
-        len(consumingFluxes) != 48
-    assert len(producingFLuxes) == 194, 'Different producingFlux'
-    with pytest.raises(AssertionError):
-        len(producingFluxes) != 194
-    assert sum(producingFluxes) == pytest.approx(9.8842775465710206e-16, abs=0.001e-16), 'Different producingFlux'
-    with pytest.raises(AssertionError):
-        sum(producingFluxes) != pytest.approx(9.8842775465710206e-16, abs=0.001e-16)
+
+    with pytest.raises(AssertionError, message = "Expecting Different Fluxes"):
+        assert len(nonZeroReactions) != 2
+    with pytest.raises(AssertionError, message = "Expecting Different Flux Into Core"):
+        assert fluxIntoCore != pytest.approx(1.1372958348092502e-15, abs=0.001e-15)
+    with pytest.raises(AssertionError, message = "Expecting Different Consuming Flux"):
+        assert len(consumingFluxes) != 48
+    with pytest.raises(AssertionError, message = "Expecting Different Producing Flux"):
+        assert len(producingFluxes) != 194
+    with pytest.raises(AssertionError, message = "Expecting Different Producing Flux"):
+        assert sum(producingFluxes) != pytest.approx(9.8842775465710206e-16, abs=0.001e-16)
+
     producingMinusConsumingFlux = sum(producingFluxes) - sum(consumingFluxes)
-    assert producingMinusConsumingFlux == pytest.approx(fluxIntoCore, abs=0.001e-15), 'Incorrect FluxIntoCore'
-    with pytest.raises(AssertionError):
-        producingMinusConsumingFlux != pytest.approx(fluxIntoCore, abs=0.001e-15)
+
+    with pytest.raises(AssertionError, message = "Expecting Different Flux Into Core"):
+        assert producingMinusConsumingFlux != pytest.approx(fluxIntoCore, abs=0.001e-15)
+    if (nonZeroReactions[0] == approx(6.0519877877298019e-32, abs=0.001e-32)):
+        with pytest.raises(AssertionError, message = "Expecting Different Fluxes"):
+            assert nonZeroReactions[1] != approx(9.8842775465710206e-16, abs=0.001e-16)
+    elif (nonZeroReactions[0] == approx(9.8842775465710206e-16, abs=0.001e-16)):
+        with pytest.raises(AssertionError, message = "Expecting Different Fluxes"):
+            assert nonZeroReactions[1] != approx(6.0519877877298019e-32, abs=0.001e-32)
+    else:
+        raise AssertionError("Expecting Different Fluxes")
